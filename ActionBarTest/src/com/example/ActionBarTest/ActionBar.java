5eafc9cb394900c90a03122f0e309ce86119ebb3
lirunzi <lirunzi@gmail.com>
2013-12-09 17:51:12 +0800

    ActionBar test

---
+++ ActionBarTest/src/com/example/ActionBarTest/ActionBar.java
@@ -0,0 +1,35 @@
+package com.example.ActionBarTest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ActionBar extends ActionBarActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_search:
+                Intent intent = new Intent(this, TwoActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return false;
+    }
+}
---
+++ ActionBarTest/src/com/example/ActionBarTest/TwoActivity.java
@@ -0,0 +1,34 @@
+package com.example.ActionBarTest;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-12-9
+ * Time: 下午5:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class TwoActivity extends ActionBarActivity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("我是2222");
+        setContentView(textView);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
